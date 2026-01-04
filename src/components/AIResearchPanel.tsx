44895410b442ed968505a896d90c0d8665d0ba38
Nyx∆ <robjcbrown@gmail.com>
2026-01-04 11:51:30 +0000

    REMOVE ALL CHAT CRAP - ONE BUTTON shows expert panel with automatic feedback

--- src/components/AIResearchPanel.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-export default function AIResearchPanel() {
-  return (
-    <div className="fixed bottom-6 right-6 z-[9999] bg-red-500 text-white p-4 rounded-lg">
-      TEST BUTTON - If you see this, React works!
-    </div>
-  );
-}
